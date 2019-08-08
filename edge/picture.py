c384f6b9b4da6c93fb7dca17e107b65bdfebd903
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-08-08 15:51:33 +0900

    Create picture.py

---
+++ edge/picture.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import time
+import picamera
+ 
+with picamera.PiCamera() as camera:
+ camera.resolution = (1024, 768)
+ camera.start_preview()
+ # Camera warm-up time
+ time.sleep(2)
+ while True:
+  time.sleep(2)
+  camera.capture('my_picture.jpg')
