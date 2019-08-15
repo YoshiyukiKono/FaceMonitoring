3404473134e02ebe48514a983e24819e596728d7
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-08-15 18:38:12 +0900

    Add files via upload

--- server/image_process.py
+++ server/image_process.py
@@ -1,4 +1,22 @@
 import cv2
 
+cascade_path = "./models/haarcascade_frontalface_default.xml"
+
 def canny(image):
     return cv2.Canny(image, 100, 200)
+
+def face(image):
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cascade = cv2.CascadeClassifier(cascade_path)
+
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+
+    color = (255, 255, 255)
+
+    if len(facerect) > 0:
+
+        for rect in facerect:
+            cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
+
+    return image
--- server/server.py
+++ server/server.py
@@ -1,24 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import numpy as np
+import cv2
+from image_process import canny, face
+from datetime import datetime
+import os
+import string
+import random
+
+import base64
+from io import BytesIO
+
+# pip install pillow
+from PIL import Image
+
+
+SAVE_DIR = "./images"
+if not os.path.isdir(SAVE_DIR):
+    os.mkdir(SAVE_DIR)
+
+app = Flask(__name__, static_url_path="")
+
+def random_str(n):
+    return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(n)])
+
+def load_image():
+    #filename = "./images/test.png"
+    filename = "./images/download.jpg"
+    img = cv2.imread(filename)
+    new_img = face(img)
+    #new_img = img
+    new_img = new_img[:, :, ::-1]
+    #pil_img = Image.fromarray(np.uint8(new_img))
+    pil_img = Image.fromarray(new_img)
+    binary = BytesIO()
+    pil_img.save(binary,"JPEG")
+    #with open(filename, 'rb') as f:
+    #    binary = f.read()
+    #pil_img.save(buf,format="png")
+    #b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
+
+    #img_binarystream = io.BytesIO(img_binary)
+    #img_pil = Image.open(img_binarystream)
+    #img_numpy = np.asarray(img_pil)
+
+    b64str = base64.b64encode(binary.getvalue()).decode("utf-8")
+    b64data = "data:image/jpg;base64,{}".format(b64str)
+    return b64data
+
 @app.route('/')
 def index():
-    return render_template('index.html', images=os.listdir(SAVE_DIR)[::-1])
+    return render_template('index.html', images=os.listdir(SAVE_DIR)[::-1], b64img=load_image())
 
 @app.route('/images/<path:path>')
 def send_js(path):
     return send_from_directory(SAVE_DIR, path)
 
-# https://qiita.com/yuuuu3/items/6e4206fdc8c83747544b
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.files['image']:
-        # 
         stream = request.files['image'].stream
         img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
         img = cv2.imdecode(img_array, 1)
 
-        #
         img = canny(img)
 
-        #
         dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_") + random_str(5)
         save_path = os.path.join(SAVE_DIR, dt_now + ".png")
         cv2.imwrite(save_path, img)
