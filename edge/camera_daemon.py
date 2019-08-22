a741f1fb9bca29fb50ddd29776ab4b3dc990c622
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-08-22 10:41:36 +0900

    Create camera_daemon.py

---
+++ edge/camera_daemon.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import time
+import os
+import sys
+
+def main_unit():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        while True:
+            camera.capture('./picture/picture.jpg')
+            shutil.copy('./picture/picture.jpg', './iot/picture.jpg')
+
+def daemonize():
+    pid = os.fork()
+    if pid > 0:
+        pid_file = open('/var/run/camera_daemon.pid','w')
+        pid_file.write(str(pid)+"\n")
+        pid_file.close()
+        sys.exit()
+    if pid == 0:
+        main_unit()
+
+if __name__ == '__main__':
+    while True:
+        daemonize()
