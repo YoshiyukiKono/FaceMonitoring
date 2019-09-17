c50736a71079d9cb7d68bb8b613595391b9bde72
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-09-17 17:14:19 +0900

    Update image_process.py

--- server/image_process.py
+++ server/image_process.py
@@ -20,20 +20,3 @@ def face(image):
             cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
 
     return image
-
-def face(image):
-    try:
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        cascade = cv2.CascadeClassifier(cascade_path)
-        facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
-
-        color = (255, 255, 255)
-
-        if len(facerect) > 0:
-            for rect in facerect:
-                cv2.rectangle(image, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), color, thickness=2)
-    except:
-        pass
-    finally:
-        return image
-
